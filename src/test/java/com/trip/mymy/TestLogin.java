9faaaf53951f0cbdede32061bf4a305f1f37da6d
Choijy <dkcl.8642@gmail.com>
2025-03-12 10:21:44 +0900

    Fix: test코드 에러 수정

--- src/test/java/com/trip/mymy/TestLogin.java
+++ src/test/java/com/trip/mymy/TestLogin.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.trip.mymy.controller.LoginController;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = {"classpath:testLogin.xml"})
 public class TestLogin {
-	@Autowired LoginController lc;
-	
+
 	@Test
 	public void testLc() {
 		
